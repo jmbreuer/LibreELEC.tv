2f2a81265a0c096a44ab3664daa13916f08f6c3d
Rudi Heitbaum <rudi@heitbaum.com>
2025-07-20 16:26:38 +0000

    kodi-binary-addons: update to latest versions
    - screensaver.asteroids: update 22.0.2-Piers to 22.0.3-Piers
    - screensaver.greynetic: update 22.0.2-Piers to 22.0.3-Piers
    - screensaver.matrixtrails: update 21.0.2-Omega to 22.0.1-Piers
    - screensaver.pyro: update 22.0.2-Piers to 22.0.3-Piers
    - screensaver.stars: update 22.0.1-Piers to 22.0.2-Piers

--- packages/mediacenter/kodi-binary-addons/screensaver.asteroids/package.mk
+++ packages/mediacenter/kodi-binary-addons/screensaver.asteroids/package.mk
@@ -3,9 +3,9 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="screensaver.asteroids"
-PKG_VERSION="22.0.2-Piers"
-PKG_SHA256="3db8595fa121290fa857bf590404cd70ae910ce90c2273477baf72194e1ae2b4"
-PKG_REV="4"
+PKG_VERSION="22.0.3-Piers"
+PKG_SHA256="2cd4546b0d89439de4b808b158860f16e0e57fe6c9e4e0815a6621caae83a8dc"
+PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/xbmc/screensaver.asteroids"
--- packages/mediacenter/kodi-binary-addons/screensaver.pyro/package.mk
+++ packages/mediacenter/kodi-binary-addons/screensaver.pyro/package.mk
@@ -3,9 +3,9 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="screensaver.pyro"
-PKG_VERSION="22.0.2-Piers"
-PKG_SHA256="1a3f19ed1328c87b63be21f6d2fe068d01c338906cad280c5630011a6caab83d"
-PKG_REV="3"
+PKG_VERSION="22.0.3-Piers"
+PKG_SHA256="fbcae6128224bf76bcb3854a01e4358fac0fc85b9b754d86129cb9d1678337d1"
+PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/xbmc/screensaver.pyro"
