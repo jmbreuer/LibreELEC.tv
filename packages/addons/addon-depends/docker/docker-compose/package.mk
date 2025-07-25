8a5177121736dd200314f1802839035fc47aacfb
Rudi Heitbaum <rudi@heitbaum.com>
2025-07-25 12:23:47 +0000

    docker-compose: update to 2.39.1

--- packages/addons/addon-depends/docker/docker-compose/package.mk
+++ packages/addons/addon-depends/docker/docker-compose/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2025-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="docker-compose"
-PKG_VERSION="2.38.1"
+PKG_VERSION="2.39.1"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://github.com/docker/compose"
 PKG_LONGDESC="Define and run multi-container applications with Docker."
@@ -10,15 +10,15 @@ PKG_TOOLCHAIN="manual"
 
 case "${ARCH}" in
   "aarch64")
-    PKG_SHA256="f7e9ed22cf330018cdc3e802e59cacb7fff3520d62aa111ac6640e00597a0ebc"
+    PKG_SHA256="7b2627ed76f7dcb0d93f649f185af912372229b4c09762a3cd1db5be5255632b"
     PKG_URL="${PKG_SITE}/releases/download/v${PKG_VERSION}/docker-compose-linux-aarch64"
     ;;
   "arm")
-    PKG_SHA256="f9d95a3bf756be4c6be661ab5799e9cb62423d24fd4bf65d9c59fa6ffbc1a525"
+    PKG_SHA256="a97c785b148cf744e4f91835fa981dc93d6ced5132d42dab934947e33032af98"
     PKG_URL="${PKG_SITE}/releases/download/v${PKG_VERSION}/docker-compose-linux-armv7"
     ;;
   "x86_64")
-    PKG_SHA256="d94a60549c6916499ce41959941e6c88b67c23ed98d1c5eee2cfc4a2d58aa24b"
+    PKG_SHA256="a5ea28722d5da628b59226626f7d6c33c89a7ed19e39f750645925242044c9d2"
     PKG_URL="${PKG_SITE}/releases/download/v${PKG_VERSION}/docker-compose-linux-x86_64"
     ;;
 esac
