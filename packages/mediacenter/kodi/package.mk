5d732d35efb2dd4054ade8e5c3455575b266f3b1
Rudi Heitbaum <rudi@heitbaum.com>
2025-07-29 14:04:35 +0000

    kodi: update to githash 20732e8
    Log:
    - https://github.com/xbmc/xbmc/compare/3ca92809f8b91de1da6b37c02cdf8df5641a3f3c...20732e8604d8c20181914deca8346376d67439e9

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="kodi"
-PKG_VERSION="3ca92809f8b91de1da6b37c02cdf8df5641a3f3c"
-PKG_SHA256="d5143a911ea5b50833a4753eee167d70fd1e56e9255d96916d59d4278f22a672"
+PKG_VERSION="20732e8604d8c20181914deca8346376d67439e9"
+PKG_SHA256="e0075016ae591a4b323c1181134cc7a24602552fefec31e794e38c316c02f003"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
 PKG_URL="https://github.com/xbmc/xbmc/archive/${PKG_VERSION}.tar.gz"
