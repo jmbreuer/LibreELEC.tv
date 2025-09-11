63812ba34be7aaa8ad1a2d0140708009ef77b3c5
Rudi Heitbaum <rudi@heitbaum.com>
2025-09-11 08:56:10 +0000

    minisatip: update to 2.0.30 and addon (10)

--- packages/addons/service/minisatip/package.mk
+++ packages/addons/service/minisatip/package.mk
@@ -2,9 +2,9 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="minisatip"
-PKG_VERSION="2.0.28"
-PKG_SHA256="2181dfd5768080ac8d41c7b1bb7302f1edd8e15cee83b921e4eb8757867fe794"
-PKG_REV="9"
+PKG_VERSION="2.0.30"
+PKG_SHA256="77436891d9698a016c4cd7e236e4a07552670f333e7b5dd9c24642852e3728a8"
+PKG_REV="10"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/catalinii/minisatip"
