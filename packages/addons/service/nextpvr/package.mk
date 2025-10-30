74f2cee34600c720fdb437b8853197ba705aff21
Martin Vallevand <mvallevand@gmail.com>
2025-10-30 13:32:57 -0400

    nextpvr: Update to 7.0.3
    https://forums.nextpvr.com/showthread.php?tid=66572

--- packages/addons/service/nextpvr/package.mk
+++ packages/addons/service/nextpvr/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="nextpvr"
-PKG_VERSION="7.0.1~Piers"
-PKG_ADDON_VERSION="7.0.1~1"
+PKG_VERSION="7.0.3~Piers"
+PKG_ADDON_VERSION="7.0.3~1"
 PKG_REV="2"
 PKG_ARCH="any"
 PKG_LICENSE="NextPVR"
