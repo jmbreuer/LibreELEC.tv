24809d087a2eaa18f935032400a2567c5decb863
Matthias Reichl <hias@horus.com>
2025-11-11 00:15:19 +0100

    rpi-eeprom: update to c6a7555
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/tools/rpi-eeprom/package.mk
+++ packages/tools/rpi-eeprom/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="rpi-eeprom"
-PKG_VERSION="e7504bafdad884d82264d906ed0b4ad8a9fd8af0"
-PKG_SHA256="adcaf9904d90b5bc55d83a404973fb0d01fee0bf0ed83e147033d7fe563a1900"
+PKG_VERSION="c6a755543ee058d9487ce3c495de5da0675fd7a3"
+PKG_SHA256="d10320eafc4818ea629364e781abdb7659257b016adcc15621ec6cce11764bd3"
 PKG_LICENSE="BSD-3/custom"
 PKG_SITE="https://github.com/raspberrypi/rpi-eeprom"
 PKG_URL="https://github.com/raspberrypi/rpi-eeprom/archive/${PKG_VERSION}.tar.gz"
