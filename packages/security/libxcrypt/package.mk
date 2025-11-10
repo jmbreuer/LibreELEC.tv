860b8bbe5c3dde647b4e1db857c8a73f22f18414
Rudi Heitbaum <rudi@heitbaum.com>
2025-11-10 21:37:30 +0000

    libxcrypt: update to 4.5.2

--- packages/security/libxcrypt/package.mk
+++ packages/security/libxcrypt/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2023-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libxcrypt"
-PKG_VERSION="4.5.1"
-PKG_SHA256="e9b46a62397c15372935f6a75dc3929c62161f2620be7b7f57f03d69102c1a86"
+PKG_VERSION="4.5.2"
+PKG_SHA256="71513a31c01a428bccd5367a32fd95f115d6dac50fb5b60c779d5c7942aec071"
 PKG_LICENSE="LGPL-2.1"
 PKG_SITE="https://github.com/besser82/libxcrypt"
 PKG_URL="https://github.com/besser82/libxcrypt/releases/download/v${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.xz"
