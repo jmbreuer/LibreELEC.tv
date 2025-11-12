53c22203e8247b8556a18bbad16b74d8156f56f6
Rudi Heitbaum <rudi@heitbaum.com>
2025-11-12 01:10:05 +0000

    filebrowser: update to 2.45.1 and addon (2)

--- packages/addons/service/filebrowser/package.mk
+++ packages/addons/service/filebrowser/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2023-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="filebrowser"
-PKG_VERSION="2.45.0"
-PKG_REV="1"
+PKG_VERSION="2.45.1"
+PKG_REV="2"
 PKG_LICENSE="Apache License 2.0"
 PKG_SITE="https://filebrowser.org"
 PKG_DEPENDS_TARGET="toolchain:host"
@@ -15,15 +15,15 @@ PKG_TOOLCHAIN="manual"
 
 case "${ARCH}" in
   "aarch64")
-    PKG_SHA256="73c593f82932c8953839e21f177287682200772ba8a6687b877ddb003a77014a"
+    PKG_SHA256="e4f9961124fa6156c4208cf46ac50bf967bfae7f7b88b99add9c7cb6739bc21f"
     PKG_URL="https://github.com/filebrowser/filebrowser/releases/download/v${PKG_VERSION}/linux-arm64-filebrowser.tar.gz"
     ;;
   "arm")
-    PKG_SHA256="1f7edde1a6f8523e75cf3e540877f228e2e8677f6a2be9ea74b3be8e54a6616d"
+    PKG_SHA256="1937ebc9610d8d67bb01887b8e8f9f850a1e94f26f017ce17952e296a29fc820"
     PKG_URL="https://github.com/filebrowser/filebrowser/releases/download/v${PKG_VERSION}/linux-armv7-filebrowser.tar.gz"
     ;;
   "x86_64")
-    PKG_SHA256="500db540a5beaf49a96abe24f142e87f4fd2c7705fe4bf219de68f82e7ee86ad"
+    PKG_SHA256="e0c07c8926bd850f8a62a221a0b6832afcbed0ff445c68a4116562c0cf0ddc84"
     PKG_URL="https://github.com/filebrowser/filebrowser/releases/download/v${PKG_VERSION}/linux-amd64-filebrowser.tar.gz"
     ;;
 esac
