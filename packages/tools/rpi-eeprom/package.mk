ab00d387e1cbffc3b228968fc88bb4d2517b85d1
Matthias Reichl <hias@horus.com>
2026-02-21 17:20:32 +0100

    rpi-eeprom: update to b24cc73
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- packages/tools/rpi-eeprom/package.mk
+++ packages/tools/rpi-eeprom/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="rpi-eeprom"
-PKG_VERSION="d9e44d8bb3b05b8e5f9f58e28fc95f1c909e924f"
-PKG_SHA256="dd73464e3efdf189edddc9d2136befd6282d6aa7ca2eb965f671a9b6ce4499c1"
+PKG_VERSION="b24cc73fbf9352281e91f0a0b1bdbf7eb8d5c548"
+PKG_SHA256="a831637ade21a446d08bfb3c13ee0d14ba8288aba403e6fab8f78180c3ef4a9d"
 PKG_LICENSE="BSD-3/custom"
 PKG_SITE="https://github.com/raspberrypi/rpi-eeprom"
 PKG_URL="https://github.com/raspberrypi/rpi-eeprom/archive/${PKG_VERSION}.tar.gz"
